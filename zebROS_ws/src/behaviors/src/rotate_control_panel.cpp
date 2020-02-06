d8077b7f065f1436c17f4feeefef9f417d652e9b
Henry Shearer <HDShearer@protonmail.com>
2020-02-06 01:40:13 +0000

    Added to Rotation Control Actionlib server and related files.

--- zebROS_ws/src/behaviors/src/rotate_control_panel.cpp
+++ zebROS_ws/src/behaviors/src/rotate_control_panel.cpp
@@ -2,7 +2,8 @@
 #include "actionlib/server/simple_action_server.h"
 #include "behavior_actions/RotatePanelAction.h"
 #include "behavior_actions/rotate_panel_override.h"
-#include "sensor_msgs/JointState.h"
+#include "controllers_2020_msgs/ControlPanelSrv.h"
+
 
 class RotatePanelAction {
 
@@ -10,13 +11,16 @@ class RotatePanelAction {
 
 
 		ros::NodeHandle nh_;
-		actionlib::SimpleActionServer<behavior_actions::RotatePanelAction> as_; //create the actionlib server
+		actionlib::SimpleActionServer<behavior_actions::RotatePanelAction> as_;
 		std::string action_name_;
-		ros::ServiceClient controller_client_; //create a ros client to send requests to the controller
+		ros::ServiceClient controller_client_;
+
 		behavior_actions::RotatePanelFeedback feedback_;
 		behavior_actions::RotatePanelResult result_;
 
-		//create subscribers to get data
+		//nh_.getParam("rotations", rotations);
+		//nh_.getParam("timeout", timeout);
+
 		ros::Subscriber joint_states_sub_;
 
 	public:
@@ -25,132 +29,119 @@ class RotatePanelAction {
 		bool preempted;
 		bool timed_out;
 
-		//make the executeCB function run every time the actionlib server is called
-		RotatePanelAction(const std::string &name) :
+	RotatePanelAction(const std::string &name) :
 			as_(nh_, name, boost::bind(&RotatePanelAction::executeCB, this, _1), false),
 			action_name_(name)
-
 	{
 
-		as_.start(); //start the actionlib server
+		as_.start();
 
-		//do networking stuff?
 		std::map<std::string, std::string> service_connection_header;
 		service_connection_header["tcp_nodelay"] = "1";
 
-		//initialize the client being used to call the controller
-		//ASK ADAM controller_client_ = nh_.serviceClient<control_panel_controller::ControlPanelSrv>("/frcrobot/controllers_2020/control_panel_controller", false, service_connection_header);
+		controller_client_ = nh_.serviceClient<controllers_2020_msgs::ControlPanelSrv>("/frcrobot_jetson/control_panel_command", false, service_connection_header);
 
-		//start subscribers subscribing
-		//ASK ADAM joint_states_sub_ = nh_.subscribe("/frcrobot_jetson/joint_states", 1, &RotatePanelAction::jointStateCallback, this);
-
+		joint_states_sub_ = nh_.subscribe("/frcrobot_jetson/talon_states", 1, &RotatePanelAction::talonStateCallback, this);
 	}
 
 		~RotatePanelAction(void)
 		{
 		}
 
-		//define the function to be executed when the actionlib server is called
-		void executeCB(const behavior_actions::RotatePanelGoalConstPtr &goal) {
-			ros::Rate r(10);
-			//define variables that will be re-used for each call to a controller
-			double start_time;
-			bool success; //if controller call succeeded
-			result_.success = true;
-			//define variables that will be set true if the actionlib action is to be ended
-			//this will cause subsequent controller calls to be skipped, if the template below is copy-pasted
-			//if both of these are false, we assume the action succeeded
-			preempted = false;
-			timed_out = false;
-
-			//ROS_INFO("%s: Executing, rotating panel %i times.", action_name_.c_str(), goal->rotations);
-
-			//send something to a controller (cargo intake in this case), copy-paste to send something else to a controller ---------------------------------------
-			if(!preempted && !timed_out)
+		void executeCB(const behavior_actions::RotatePanelGoalConstPtr &goal)
+			if(!control_panel_controller_client_.waitForExistence(ros::Duration(wait_for_server_timeout)))
 			{
-				ROS_INFO("Rotate Panel Server: Rotating Panel");
+				ROS_ERROR_STREAM("Rotate Panel Server can't find control_panel_controller");
+				as_.setPreempted();
+				return 0;
+			}
+		void talonStateCallback()
+		{
+			int i = 1;
+		}
 
-				//reset variables
-				start_time = ros::Time::now().toSec();
-				success = false;
+		ros::Rate r(100);
 
-				//define request to send to cargo intake controller
-				//HELP rotate_panel_controller::RotatePanelSrv srv;
-				//HELP srv.request.rotations = goal->rotations;
+		double start_time;
+		bool success;
 
-				//send request to controller
-				/* ASK ADAM if(!controller_client_.call(srv)) //note: the call won't happen if preempted was true, because of how && operator works
-				   {
-				   ROS_ERROR("Srv intake call failed in auto interpreter server intake");
-				   }*/
-				//update everything by doing spinny stuff
+		controllers_2020_msgs::ControlPanelSrv srv;
 
-				ros::spinOnce();
+		result_.success = true;
+		preempted = false;
+		timed_out = false;
 
-				//run a loop to wait for the controller to do its work. Stop if the action succeeded, if it timed out, or if the action was preempted
-				while(!success && !timed_out && !preempted) {
-					if(as_.isPreemptRequested() || !ros::ok()) {
-						ROS_WARN("%s: Preempted", action_name_.c_str());
-						as_.setPreempted();
-						preempted = true;
-					}
-					if (!preempted) {
-						r.sleep();
-						ros::spinOnce();
-						timed_out = (ros::Time::now().toSec()-start_time) > timeout;
-					}
+		if(!preempted && !timed_out)
+		{
+			ROS_INFO("Rotate Panel Server: Rotating Panel");
+
+			start_time = ros::Time::now().toSec();
+			success = false;
+
+			srv.request.control_panel_rotations = 3.3; //add env variable stuff
+			srv.request.panel_arm_extend = true;
+
+			if(!controller_client_.call(srv))
+			{
+				ROS_ERROR("Srv intake call failed in auto interpreter server intake");
+				preempted = true;
+			}
+
+			while(!success && !timed_out && !preempted) {
+				if(as_.isPreemptRequested() || !ros::ok()) {
+					ROS_WARN("%s: Preempted", action_name_.c_str());
+					as_.setPreempted();
+					preempted = true;
+				}
+				if (!preempted) {
+					r.sleep();
+					ros::spinOnce();
+					timed_out = (ros::Time::now().toSec()-start_time) > timeout;
 				}
 			}
-			//end of code for sending something to a controller. Other controller calls and as servers go here
-
-
-			//log state of action and set result of action
-			if(timed_out)
-			{
-				ROS_INFO("%s: Timed Out", action_name_.c_str());
-			}
-			else if(preempted)
-			{
-				ROS_INFO("%s: Preempted", action_name_.c_str());
-			}
-			else //implies succeeded
-			{
-				ROS_INFO("%s: Succeeded", action_name_.c_str());
-			}
-
-			result_.timed_out = timed_out; //timed_out refers to last controller call, but applies for whole action
-			result_.success = success; //success refers to last controller call, but applies for whole action
-			as_.setSucceeded(result_); //pretend it succeeded no matter what, but tell what actually happened with the result - helps with SMACH
-			return;
 		}
 
-		bool DS_Override(behavior_actions::rotate_panel_override::Request &req,
-				behavior_actions::rotate_panel_override::Response &res)
+		if(timed_out)
 		{
-			if (req.override == true)
-			{
-				preempted = true;
-				res.success = true;
-				res.override = true;
-				return true;
-			}
-
-			else
-			{
-				return false;
-			}
+			ROS_INFO("%s: Timed Out", action_name_.c_str());
 		}
+		else if(preempted)
+		{
+			ROS_INFO("%s: Preempted", action_name_.c_str());
+		}
+		else
+		{
+			ROS_INFO("%s: Succeeded", action_name_.c_str());
+		}
+
+		result_.timed_out = timed_out;
+		result_.success = success;
+		as_.setSucceeded(result_);
+		return true;
 };
+
+bool DS_Override(behavior_actions::rotate_panel_override::Request &req,
+		behavior_actions::rotate_panel_override::Response &res)
+{
+	if (req.override == true)
+	{
+		//RotatePanelAction.preempted = true;
+		res.success = true;
+		res.override = true;
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "rotate_panel_server");
 	RotatePanelAction rotate_panel_action("rotate_panel_server");
 	ros::NodeHandle n;
 	ros::NodeHandle n_params(n, "teleop_params");
-	ros::NodeHandle n_params(n, "rotateHatchPanelParams");
-	if(!n_params.getParam("rotations", rotations))
-        ROS_ERROR_STREAM("Could not read rotations in rotateHatchPanel");
-	if(!n_params.getParam("timeout", timeout))
-        ROS_ERROR_STREAM("Could not read timeout in rotateHatchPanel");
+	ros::NodeHandle n_rotate_params(n, "rotateHatchPanelParams");
 	ros::spin();
 	return 0;
 }
