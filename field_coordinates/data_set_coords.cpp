a5da4a940bcd427cc31282d831bbf3fff6e7b048
Theo <mr.unlimited3141@gmail.com>
2021-02-16 16:51:44 +0000

    A few polishing touches, and caught a bug on line 51 where I used the wrong input

--- field_coordinates/data_set_coords.cpp
+++ field_coordinates/data_set_coords.cpp
@@ -6,7 +6,7 @@
 struct point {
   double metric_x = -1; //return horizontal distance in cm
   double metric_y = -1; //return vertical distance in cm
-} point_1;
+};
 constexpr char min_y = 'A'; //highest row of "actual" points
 constexpr char top_row = 'T'; //edge above A
 std::vector<std::string> bad_coords = {"A2","A5","A11","E2","E5","E11","C1","C2","C4","C5","C6","C7","C8","C9","C10","C11"}; //Any points that do not exist
@@ -23,7 +23,6 @@ bool user_friendly (int x_, char& y_){
   else if (y_ == top_row)
   {
     y_ = 64; //sets the y_coord in convert_to_metric to the correct ASCII equivalent, allows for changes to top_row
-    answer = true;
   }
   else if (y_ < min_y || y_ > max_y)
   {
@@ -49,7 +48,7 @@ bool convert_to_metric(int x_coord, char y_coord, point& input){
   {
     input.metric_x = (x_coord*grid_dist);
     input.metric_y = (max_y_dist-(y_coord-min_y)*grid_dist);
-    std::cout<<point_1.metric_x<<" cm, "<<point_1.metric_y<<" cm"<<std::endl;
+    std::cout<<input.metric_x<<" cm, "<<input.metric_y<<" cm"<<std::endl;
     return true;
   }
   else //error message on a bad input
@@ -69,6 +68,7 @@ bool convert_to_metric(int x_coord, char y_coord, point& input){
 }
 
 int main(int argc, char const *argv[]){
+  point point_1;
   char y;
   int x;
   std::cout<<"Input field coordinates"<<std::endl;
